368cbb2d8c6c39e5011fa433d609982df2e2ab05
mcanouil <mickael.canouil@cnrs.fr>
2018-04-11 15:36:26 +0200

    Change figures size; load all libraries at beginning

--- RunAnalysis.sh
+++ RunAnalysis.sh
@@ -7,24 +7,24 @@ wd <- './'
 
 # Analysis
 rmarkdown::render(
-    input = 'RHAPSODY_WP3_PreDiab.Rmd', 
-    output_format = "html_document", 
-    output_dir = wd,
-    params = list(
-        cohort_name = 'CohortName',
-        author_name = 'Firstname LASTNAME',
-        opal_server = 'http://localhost:8080',
-        opal_login = 'administrator', 
-        opal_password = 'password', 
-        output_directory = wd,
-        vcf_input_directory = './vcfs',
-        imputation_quality_tag = 'INFO', 
-        output_code = FALSE, 
-        vcftools_binary_path = '/vcftools/vcftools_latest/bin', 
-        format_vcfs = FALSE,
-        variants_analysis = FALSE
-    ),
-    encoding = 'UTF-8'
+  input = 'RHAPSODY_WP3_PreDiab.Rmd', 
+  output_format = "html_document", 
+  output_dir = wd,
+  params = list(
+    cohort_name = 'CohortName',
+    author_name = 'Firstname LASTNAME',
+    opal_server = 'http://localhost:8080',
+    opal_login = 'administrator', 
+    opal_password = 'password', 
+    output_directory = wd,
+    vcf_input_directory = './vcfs',
+    imputation_quality_tag = 'INFO', 
+    output_code = FALSE, 
+    vcftools_binary_path = '/vcftools/vcftools_latest/bin', 
+    format_vcfs = FALSE,
+    variants_analysis = FALSE
+  ),
+  encoding = 'UTF-8'
 )
 
 RSCRIPT
--- handleVCF.sh
+++ handleVCF.sh
@@ -29,89 +29,89 @@ echo "name of imputation quality tag (INFO/*): $imputationQualityTag"
 cat $vcfout/vcffilespath.txt | while read vcf
 do
 
-    base=`basename $vcf`
-    echo "name of inputFile: $base"
+  base=`basename $vcf`
+  echo "name of inputFile: $base"
 
-    ##directory for extraction from vcf (dosage, freq, etc). and for the small vcf files
-    chrDir=`basename $vcf .vcf.gz`
-    eVCF=$vcfout/$chrDir/extractVCF
-    sVCF=$vcfout/$chrDir/smallVCF
-    mkdir -p $eVCF
-    mkdir -p $sVCF
-
-
-    ##clean up before we start
-    rm -f $eVCF/*
-    rm -f $sVCF/*
+  ##directory for extraction from vcf (dosage, freq, etc). and for the small vcf files
+  chrDir=`basename $vcf .vcf.gz`
+  eVCF=$vcfout/$chrDir/extractVCF
+  sVCF=$vcfout/$chrDir/smallVCF
+  mkdir -p $eVCF
+  mkdir -p $sVCF
 
 
-    ############# step 1: Delete the obviuos problematic SNPs, indels, and rare SNPs
-    echo -e "\n"
-    echo "Reduce bad SNPs in $base ... (takes a while if many SNPs)"
-    $vcftoolsPath/vcftools --gzvcf $vcf \
-        --get-INFO $imputationQualityTag \
-        --out $eVCF/tmp
-    awk '{if($5<0.3) print $1"\t"$2}' $eVCF/tmp.INFO > $eVCF/delme
-    $vcftoolsPath/vcftools --gzvcf $vcf \
-        --exclude-positions $eVCF/delme \
-        --maf 0.01 \
-        --remove-indels \
-        --remove-filtered-all \
-        --recode-INFO-all \
-        --recode \
-        --stdout | gzip -c > $eVCF/filtered.$base
+  ##clean up before we start
+  rm -f $eVCF/*
+  rm -f $sVCF/*
 
 
-    ############ step 2: Break down the vcf file into smaller files.
-    ############ Make a header and attached the header to all Files
-    # gunzip -c  $eVCF/filtered.$base | awk '{if (NR<109) print $0}' > $sVCF/tmpHeader
-    gunzip -c $eVCF/filtered.$base | head -n 200 | awk '/^#/' > $sVCF/tmpHeader
-
-    ##unzip vcf and keep all but header. Split the files into lines of 1000 and call them pre*
-    # gunzip -c  $eVCF/filtered.$base | awk '{if (NR>12) print $0}' |  split - -l1000 $sVCF/pre
-    gunzip -c $eVCF/filtered.$base | awk '!/^#/' |  split - -l1000 $sVCF/pre
-
-    ##loop through the pre*, add the header and zip the file. Save as "small_*.vcf.gz"
-    find $sVCF/pre* | while read pathToFile;
-    do
-        file=`basename $pathToFile`
-        cat $sVCF/tmpHeader $sVCF/$file | gzip -c > $sVCF/small_${file}.vcf.gz
-    done
-
-
-    ########### step 3: is DS available?
-    ###maybe we should check if dosage is available for all cohorts before we make a script here.
+  ############# step 1: Delete the obviuos problematic SNPs, indels, and rare SNPs
+  echo -e "\n"
+  echo "Reduce bad SNPs in $base ... (takes a while if many SNPs)"
+  $vcftoolsPath/vcftools --gzvcf $vcf \
+    --get-INFO $imputationQualityTag \
+    --out $eVCF/tmp
+  awk '{if($5<0.3) print $1"\t"$2}' $eVCF/tmp.INFO > $eVCF/delme
+  $vcftoolsPath/vcftools --gzvcf $vcf \
+    --exclude-positions $eVCF/delme \
+    --maf 0.01 \
+    --remove-indels \
+    --remove-filtered-all \
+    --recode-INFO-all \
+    --recode \
+    --stdout | gzip -c > $eVCF/filtered.$base
 
 
+  ############ step 2: Break down the vcf file into smaller files.
+  ############ Make a header and attached the header to all Files
+  # gunzip -c  $eVCF/filtered.$base | awk '{if (NR<109) print $0}' > $sVCF/tmpHeader
+  gunzip -c $eVCF/filtered.$base | head -n 200 | awk '/^#/' > $sVCF/tmpHeader
 
-    ########### step 4: Extract dosage, impute, freq from each od the small VCFs
-    echo -e "\n"
-    echo extract dosage, R2, freq...
+  ##unzip vcf and keep all but header. Split the files into lines of 1000 and call them pre*
+  # gunzip -c  $eVCF/filtered.$base | awk '{if (NR>12) print $0}' |  split - -l1000 $sVCF/pre
+  gunzip -c $eVCF/filtered.$base | awk '!/^#/' |  split - -l1000 $sVCF/pre
 
-    find $sVCF/small* | while read pathToFile
-    do
-        file=`basename $pathToFile .vcf.gz`
-
-        $vcftoolsPath/vcftools --gzvcf $pathToFile \
-            --extract-FORMAT-info DS \
-            --stdout | gzip -c > $eVCF/$file.DS.gz
-        $vcftoolsPath/vcftools --gzvcf $pathToFile \
-            --get-INFO $imputationQualityTag \
-            --out $eVCF/$file
-        $vcftoolsPath/vcftools --gzvcf $pathToFile \
-            --freq \
-            --out $eVCF/$file
-        sed -i s/"{ALLELE:FREQ}"/frq1"\t"frq2/ $eVCF/$file.frq
-    done
+  ##loop through the pre*, add the header and zip the file. Save as "small_*.vcf.gz"
+  find $sVCF/pre* | while read pathToFile;
+  do
+    file=`basename $pathToFile`
+    cat $sVCF/tmpHeader $sVCF/$file | gzip -c > $sVCF/small_${file}.vcf.gz
+  done
 
 
-    ########## step 5: clean up
-    rm -f $eVCF/filtered.$base
-    rm -f $sVCF/tmpHeader
-    rm -f $sVCF/pre*
-    rm -f $eVCF/delme
-    rm -f $eVCF/tmp.INFO
-    # rm -f filtered.$base
-    # rm -f tmp.$base
+  ########### step 3: is DS available?
+  ###maybe we should check if dosage is available for all cohorts before we make a script here.
+
+
+
+  ########### step 4: Extract dosage, impute, freq from each od the small VCFs
+  echo -e "\n"
+  echo extract dosage, R2, freq...
+
+  find $sVCF/small* | while read pathToFile
+  do
+    file=`basename $pathToFile .vcf.gz`
+
+    $vcftoolsPath/vcftools --gzvcf $pathToFile \
+      --extract-FORMAT-info DS \
+      --stdout | gzip -c > $eVCF/$file.DS.gz
+    $vcftoolsPath/vcftools --gzvcf $pathToFile \
+      --get-INFO $imputationQualityTag \
+      --out $eVCF/$file
+    $vcftoolsPath/vcftools --gzvcf $pathToFile \
+      --freq \
+      --out $eVCF/$file
+    sed -i s/"{ALLELE:FREQ}"/frq1"\t"frq2/ $eVCF/$file.frq
+  done
+
+
+  ########## step 5: clean up
+  rm -f $eVCF/filtered.$base
+  rm -f $sVCF/tmpHeader
+  rm -f $sVCF/pre*
+  rm -f $eVCF/delme
+  rm -f $eVCF/tmp.INFO
+  # rm -f filtered.$base
+  # rm -f tmp.$base
 
 done
