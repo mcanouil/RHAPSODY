74778a233c3fc197a6b69c7452d162018f3f29f7
Mickaël Canouil <mickael.canouil@cnrs.fr>
2018-12-26 17:50:36 +0100

    1.1.0 update

--- docker_rhapsody.sh
+++ docker_rhapsody.sh
@@ -7,16 +7,27 @@
 # docker login
 
 # Build image
-docker build --tag biostatgood/rhapsody:1.0.6 --compress /media/Project/RHAPSODY/Scripts/docker
+docker build --tag umr8199/rhapsody:1.1.0 --compress /media/Project/RHAPSODY/Scripts/docker
 
 # Tag image with name and version
-docker tag biostatgood/rhapsody:1.0.6 biostatgood/rhapsody:latest
+docker tag umr8199/rhapsody:1.1.0 biostatgood/rhapsody:latest
 
 # Push image to docker hub
-docker push biostatgood/rhapsody:1.0.6
-docker push biostatgood/rhapsody:latest
+docker push umr8199/rhapsody:1.1.0
+docker push umr8199/rhapsody:latest
 
 # docker system prune --all
+
+cd /disks/PROJECT/RHAPSODY/Data/
+git clone https://gist.github.com/15362a96c1561bb51af98760b41c478e.git v1.1.0
+cd v1.1.0
+git clone https://gist.github.com/f3e2fdc59757fd8577abfe233854580a.git utils
+git clone https://gist.github.com/de2b8293fbb1d0d950881cf6290e78c4.git docker
+tar zcvf RAPSODY_WP3_Pre-Diabetes_v1.1.0.tar.gz *
+/disks/DATA/Softwares/FEX/fexsend RAPSODY_WP3_Pre-Diabetes_v1.1.0.tar.gz mickael.canouil@cnrs.fr
+cd /disks/PROJECT/RHAPSODY/Data/
+rm -Rf v1.1.0
+
 
 # Run image
 # docker run \
@@ -26,7 +37,7 @@ docker push biostatgood/rhapsody:latest
 # --volume /from/local/directory/:/to/docker/directory/target \
 # --publish 22:22 \
 # --publish 8787:8787 \
-# biostatgood/rhapsody:1.0.0
+# umr8199/rhapsody:latest
 
 
 
@@ -41,5 +52,5 @@ docker push biostatgood/rhapsody:latest
 # --volume /media/Datatmp:/media/DATATMP \
 # --publish 8791:8787 \
 # --publish 2291:22 \
-# biostatgood/rhapsody:1.0.0
+# umr8199/rhapsody:latest
 # docker ps -a
