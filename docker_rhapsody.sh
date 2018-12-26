0d81be8e352d50e132ab446a40312bcf7d3e77de
Mickaël Canouil <mickael.canouil@cnrs.fr>
2018-12-26 18:03:14 +0100

    change permissions of home scripts

--- docker_rhapsody.sh
+++ docker_rhapsody.sh
@@ -10,7 +10,7 @@
 docker build --tag umr8199/rhapsody:1.1.0 --compress /media/Project/RHAPSODY/Scripts/docker
 
 # Tag image with name and version
-docker tag umr8199/rhapsody:1.1.0 biostatgood/rhapsody:latest
+docker tag umr8199/rhapsody:1.1.0 umr8199/rhapsody:latest
 
 # Push image to docker hub
 docker push umr8199/rhapsody:1.1.0
@@ -18,15 +18,15 @@ docker push umr8199/rhapsody:latest
 
 # docker system prune --all
 
-cd /disks/PROJECT/RHAPSODY/Data/
-git clone https://gist.github.com/15362a96c1561bb51af98760b41c478e.git v1.1.0
-cd v1.1.0
-git clone https://gist.github.com/f3e2fdc59757fd8577abfe233854580a.git utils
-git clone https://gist.github.com/de2b8293fbb1d0d950881cf6290e78c4.git docker
-tar zcvf RAPSODY_WP3_Pre-Diabetes_v1.1.0.tar.gz *
-/disks/DATA/Softwares/FEX/fexsend RAPSODY_WP3_Pre-Diabetes_v1.1.0.tar.gz mickael.canouil@cnrs.fr
-cd /disks/PROJECT/RHAPSODY/Data/
-rm -Rf v1.1.0
+# cd /tmp/
+# git clone https://gist.github.com/15362a96c1561bb51af98760b41c478e.git v1.1.0
+# cd v1.1.0
+# git clone https://gist.github.com/f3e2fdc59757fd8577abfe233854580a.git utils
+# git clone https://gist.github.com/de2b8293fbb1d0d950881cf6290e78c4.git docker
+# tar zcvf RAPSODY_WP3_Pre-Diabetes_v1.1.0.tar.gz *
+# rm -Rf *v1.1.0*
+# /media/Data/Softwares/FEX/fexsend RAPSODY_WP3_Pre-Diabetes_v1.1.0.tar.gz mickael.canouil@cnrs.fr
+
 
 
 # Run image
