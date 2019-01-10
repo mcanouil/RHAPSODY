ea98885b56b68d93fd3cd399494667255e3720ea
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-01-10 12:52:37 +0100

    move parameters to the top

--- RunAnalysis.R
+++ RunAnalysis.R
@@ -2,18 +2,27 @@
 # output will be generated where the Rmarkdown file is
 working_directory <- './' 
 
-analysis_step <- 7
 cohort_name <- 'Cohort_Name'
 author_name <- 'Firstname LASTNAME'
+
+n_cpu <- 2
+
 opal_credentials <- 'opal_credentials.txt'
+
 vcf_directory <- '/media/.../vcf'
 imputation_quality_tag <- 'INFO' # To be set according to VCF (could also be 'R2')
+vcftools_binary_path <- '/usr/local/bin'
+format_vcfs <- TRUE
+
+analysis_step <- 7
+variants_analysis <- TRUE
+
 
 
 # Run the analysis
 dir.create(path = working_directory, showWarnings = FALSE, mode = '0777')
 rmarkdown::render(
-  input = '/home/rhapsody/WP3/scripts/RHAPSODY_WP3_PreDiab.Rmd', 
+  input = '~/WP3/scripts/RHAPSODY_WP3_PreDiab.Rmd', 
   output_format = 'html_document', 
   output_file = paste0(
     'RHAPSODY_WP3_PreDiab_', 
@@ -27,13 +36,13 @@ rmarkdown::render(
     opal_credentials = opal_credentials,
     vcf_input_directory = vcf_directory, 
     imputation_quality_tag = imputation_quality_tag, 
-    vcftools_binary_path = '/usr/local/bin',
+    vcftools_binary_path = vcftools_binary_path,
     output_directory = working_directory,
     analysis_step = analysis_step,
-    format_vcfs = TRUE,
-    variants_analysis = TRUE,
+    format_vcfs = format_vcfs,
+    variants_analysis = variants_analysis,
     chunk_size = 1000,
-    n_cpu = 2,
+    n_cpu = n_cpu,
     echo = FALSE, # Should R code be printed in the report
     warning = FALSE, # Should warnings be printed in the report
     message = FALSE # Should messages be printed in the report
