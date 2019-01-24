f26d5a7831ce2205294ed5e28091b1b3b6423143
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-01-24 17:12:54 +0100

    Make Dockerfile to pull scripts

--- docker_rhapsody.sh
+++ docker_rhapsody.sh
@@ -8,23 +8,23 @@
 
 
 # Build image
-docker build --tag umr8199/rhapsody:1.1.0 --compress /media/Project/RHAPSODY/Scripts/docker
+docker build --tag umr8199/rhapsody:1.1.1 --compress --file /media/Project/RHAPSODY/Scripts/docker/Dockerfile_update /media/Project/RHAPSODY/Scripts/docker/
 # Tag image with name and version
-docker tag umr8199/rhapsody:1.1.0 umr8199/rhapsody:latest
+docker tag umr8199/rhapsody:1.1.1 umr8199/rhapsody:latest
 # Push image to docker hub
-docker push umr8199/rhapsody:1.1.0
+docker push umr8199/rhapsody:1.1.1
 docker push umr8199/rhapsody:latest
 
 # docker system prune --all
 
 
-git clone https://gist.github.com/15362a96c1561bb51af98760b41c478e.git /tmp/v1.1.0
-git clone https://gist.github.com/f3e2fdc59757fd8577abfe233854580a.git /tmp/v1.1.0/utils
-git clone https://gist.github.com/de2b8293fbb1d0d950881cf6290e78c4.git /tmp/v1.1.0/docker
-cd /tmp/v1.1.0
-tar zcvf RAPSODY_WP3_Pre-Diabetes_v1.1.0.tar.gz *
-/disks/DATA/Softwares/FEX/fexsend RAPSODY_WP3_Pre-Diabetes_v1.1.0.tar.gz mickael.canouil@cnrs.fr
-cd ~; rm -Rf /tmp/v1.1.0
+git clone https://gist.github.com/15362a96c1561bb51af98760b41c478e.git /tmp/v1.1.1
+git clone https://gist.github.com/f3e2fdc59757fd8577abfe233854580a.git /tmp/v1.1.1/utils
+git clone https://gist.github.com/de2b8293fbb1d0d950881cf6290e78c4.git /tmp/v1.1.1/docker
+cd /tmp/v1.1.1
+tar zcvf RAPSODY_WP3_Pre-Diabetes_v1.1.1.tar.gz *
+/disks/DATA/Softwares/FEX/fexsend RAPSODY_WP3_Pre-Diabetes_v1.1.1.tar.gz mickael.canouil@cnrs.fr
+cd ~; rm -Rf /tmp/v1.1.1
 
 
 
