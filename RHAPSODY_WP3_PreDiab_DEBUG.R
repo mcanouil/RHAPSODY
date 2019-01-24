9cc7091b00d120ec595e89d9348fbcf2d34fe265
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-01-24 15:25:41 +0100

    keep only samples who passed QC in vcf formatting

--- RHAPSODY_WP3_PreDiab_DEBUG.R
+++ RHAPSODY_WP3_PreDiab_DEBUG.R
@@ -2,7 +2,7 @@
 # Name - RHAPSODY_WP3_PreDiab_DEBUG
 # Desc - Copy of R code from 'RHAPSODY_WP3_PreDiab.Rmd'
 # Author - Mickaël Canouil, Ph.D.
-# Version - 1.1.0
+# Version - 1.1.1
 #---------------------------------------------------------------------------------------------------
 options(stringsAsFactors = FALSE)
 
