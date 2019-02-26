731925aac87918dc0d017b8ccda9d2c28220a141
Mickaël Canouil <mickael.canouil@cnrs.fr>
2019-02-26 13:02:23 +0100

    restyle; add prefix packages; v1.2.2

--- RHAPSODY_WP3_PreDiab_DEBUG.R
+++ RHAPSODY_WP3_PreDiab_DEBUG.R
@@ -20,87 +20,43 @@ opal_credentials <- as.data.frame(t(read.table(
 ###############
 # Functions
 ###############
-# reference table to convert HbA1c from percentage to mmol/mol
-convert_hba1c <- function(x, unitFrom = "%") {
-  if (unitFrom == "%") {
-    hba1c_unit_table <- structure(list(
-      V1 = c(
-        10L, 12L, 14L, 16L, 18L, 20L, 22L, 24L,
-        26L, 28L, 30L, 32L, 34L, 36L, 38L, 40L, 42L, 44L, 46L, 48L, 50L,
-        52L, 54L, 56L, 58L, 60L, 62L, 64L, 66L, 68L, 70L, 72L, 74L, 76L,
-        78L, 80L, 82L, 84L, 86L, 88L, 90L, 92L, 94L, 96L, 98L, 100L,
-        102L, 104L, 106L, 108L, 110L, 112L, 114L, 116L, 118L, 120L, 122L,
-        124L, 126L, 128L, 130L, 132L, 134L, 136L, 138L, 140L, 142L, 144L,
-        146L, 148L, 150L, 152L, 154L, 156L, 158L, 160L, 162L, 164L, 166L,
-        168L, 170L, 172L, 174L, 176L, 178L, 180L, 182L, 184L, 186L, 188L,
-        190L, 192L, 194L, 196L, 198L, 200L, 202L, 204L, 208L, 210L
-      ),
-      V2 = c(
-        3.1, 3.2, 3.4, 3.6, 3.8, 4, 4.2, 4.3, 4.5, 4.7, 4.9,
-        5.1, 5.3, 5.4, 5.6, 5.8, 6, 6.2, 6.4, 6.5, 6.7, 6.9, 7.1,
-        7.3, 7.5, 7.6, 7.8, 8, 8.2, 8.4, 8.6, 8.7, 8.9, 9.1, 9.3,
-        9.5, 9.7, 9.8, 10, 10.2, 10.4, 10.6, 10.8, 10.9, 11.1, 11.3,
-        11.5, 11.7, 11.8, 12, 12.2, 12.4, 12.6, 12.8, 12.9, 13.1,
-        13.3, 13.5, 13.7, 13.9, 14, 14.2, 14.4, 14.6, 14.8, 15, 15.1,
-        15.3, 15.5, 15.7, 15.9, 16.1, 16.2, 16.4, 16.6, 16.8, 17,
-        17.2, 17.3, 17.5, 17.7, 17.9, 18.1, 18.3, 18.4, 18.6, 18.8,
-        19, 19.2, 19.4, 19.5, 19.7, 19.9, 20.1, 20.3, 20.4, 20.6,
-        20.8, 21, 21.2
-      )
-    ), .Names = c("V1", "V2"), class = "data.frame", row.names = c(NA, -100L))
-    hba1c_model <- coef(lm(V1~V2, data = hba1c_unit_table))
-    x <- sapply(x, function(y) round(sum(hba1c_model * c(1, y))))
-  }
-  x
-}
-
-# function to convert g/l to mmol/l
-convert_2mmoll <- function(x, from = "g/l") {
-  switch(EXPR = tolower(from),
-    "g/l" = {x / 0.18},
-    "mg/l" = {x / 0.18 / 100},
-    "mmol/l" = {x},
-    stop("Please check Units: g/l, g/L, mg/l, mg/L, mmol/l or mmol/L!")
-  )
-}
-
 # function to convert "Vital Signs" (VS) OPAL table (CDISC) to "long format"
 format_vs <- function(data) {
-  data <- mutate(
+  data <- dplyr::mutate(
     .data = data,
     KEY_FACTOR = paste(DOMAIN, STUDYID, SUBJID, VISIT, sep = "_")
   )
   rownames(data) <- NULL
   
-  descriptive_data <- distinct(.data = select(.data = data, DOMAIN, STUDYID, VSORRESU, VSTEST, VSTESTCD))
+  descriptive_data <- dplyr::distinct(.data = dplyr::select(.data = data, DOMAIN, STUDYID, VSORRESU, VSTEST, VSTESTCD))
   
   if (!"VSTPTNUM"%in%colnames(data)) {
-    data <- mutate(.data = data, VSTPTNUM = NA)
+    data <- dplyr::mutate(.data = data, VSTPTNUM = NA)
   }
   if (!"VSTPT"%in%colnames(data)) {
-    data <- mutate(.data = data, VSTPT = NA)
+    data <- dplyr::mutate(.data = data, VSTPT = NA)
   }
   
-  data_values <- full_join(
-    x = full_join(
+  data_values <- dplyr::full_join(
+    x = dplyr::full_join(
       x = data %>%
         subset(is.na(VSTPTNUM)) %>%
-        select(KEY_FACTOR, VSORRES, VSTESTCD) %>%
-        spread(key = "VSTESTCD", value = "VSORRES"),
+        dplyr::select(KEY_FACTOR, VSORRES, VSTESTCD) %>%
+        tidyr::spread(key = "VSTESTCD", value = "VSORRES"),
       y = data %>%
         subset(!is.na(VSTPTNUM)) %>%
-        select(KEY_FACTOR, VSORRES, VSTESTCD, VSTPTNUM) %>%
-        mutate(KEY_MEASURES = paste(VSTESTCD, VSTPTNUM, sep = "_")) %>%
-        select(KEY_FACTOR, KEY_MEASURES, VSORRES) %>%
-        spread(key = "KEY_MEASURES", value = "VSORRES"),
+        dplyr::select(KEY_FACTOR, VSORRES, VSTESTCD, VSTPTNUM) %>%
+        dplyr::mutate(KEY_MEASURES = paste(VSTESTCD, VSTPTNUM, sep = "_")) %>%
+        dplyr::select(KEY_FACTOR, KEY_MEASURES, VSORRES) %>%
+        tidyr::spread(key = "KEY_MEASURES", value = "VSORRES"),
       by = c("KEY_FACTOR" = "KEY_FACTOR")
     ),
     y = data %>%
-      select(-c(VSORRES, VSTPT, VSTPTNUM, VSORRESU, VSTEST, VSTESTCD)) %>%
-      distinct(),
+      dplyr::select(-c(VSORRES, VSTPT, VSTPTNUM, VSORRESU, VSTEST, VSTESTCD)) %>%
+      dplyr::distinct(),
     by = c("KEY_FACTOR" = "KEY_FACTOR")
   ) %>%
-    mutate(
+    dplyr::mutate(
       VISIT = factor(
         x = VISIT,
         levels = c(
@@ -110,9 +66,9 @@ format_vs <- function(data) {
         )
       )
     ) %>%
-    group_by(DOMAIN, STUDYID, SUBJID) %>%
-    arrange(VISIT) %>%
-    select(-KEY_FACTOR) %>%
+    dplyr::group_by(DOMAIN, STUDYID, SUBJID) %>%
+    dplyr::arrange(VISIT) %>%
+    dplyr::select(-KEY_FACTOR) %>%
     data.frame()
   
   list(data = data_values, annotation = descriptive_data)
@@ -121,24 +77,24 @@ format_vs <- function(data) {
 # function to convert "Laboratory Measurements" (LB) OPAL table (CDISC) to "long format"
 format_lb <- function(data) {
   check_multiple_hba1c <- data %>% 
-    select(LBTESTCD, LBORRESU) %>% 
-    filter(LBTESTCD=="HBA1C") %>% 
-    distinct()
+    dplyr::select(LBTESTCD, LBORRESU) %>% 
+    dplyr::filter(LBTESTCD=="HBA1C") %>% 
+    dplyr::distinct()
   if (nrow(check_multiple_hba1c)>1) {
     if (any(grepl("%", check_multiple_hba1c[["LBORRESU"]]))) {
       data <- data %>% 
-        filter(
+        dplyr::filter(
           !(LBTESTCD=="HBA1C" & LBORRESU %in% setdiff(check_multiple_hba1c[["LBORRESU"]], "%"))
         )
     } else {
        data <- data %>% 
-        filter(
+        dplyr::filter(
           !(LBTESTCD=="HBA1C" & LBORRESU %in% setdiff(check_multiple_hba1c[["LBORRESU"]], "mmol/mol"))
         )
     }
   }
   
-  data <- mutate(
+  data <- dplyr::filter(
     .data = data,
     LBFAST = if ("LBFAST" %in% colnames(data)) LBFAST else NA,
     LBTPTNUM = if ("LBTPTNUM" %in% colnames(data)) LBTPTNUM else NA,
@@ -153,26 +109,26 @@ format_lb <- function(data) {
   )
   
   descriptive_data <- data %>%
-    select(intersect(c("DOMAIN", "STUDYID", !!descriptive_variables), colnames(.))) %>%
-    distinct() %>%
-    mutate(
+    dplyr::select(intersect(c("DOMAIN", "STUDYID", !!descriptive_variables), colnames(.))) %>%
+    dplyr::distinct() %>%
+    dplyr::mutate(
       KEY_MEASURES = paste(LBTESTCD, LBSPEC, LBCAT, LBTPTNUM, sep = "_")
     )
 
-  data_values <- full_join(
+  data_values <- dplyr::full_join(
     x = (data %>%
-      select(KEY_FACTOR, LBORRES, LBTESTCD, LBSPEC, LBCAT, LBTPTNUM) %>%
-      mutate(
+      dplyr::select(KEY_FACTOR, LBORRES, LBTESTCD, LBSPEC, LBCAT, LBTPTNUM) %>%
+      dplyr::mutate(
         KEY_MEASURES = paste(LBTESTCD, LBSPEC, LBCAT, LBTPTNUM, sep = "_")
       ) %>%
-      select(KEY_FACTOR, KEY_MEASURES, LBORRES) %>%
-      spread(key = "KEY_MEASURES", value = "LBORRES")),
+      dplyr::select(KEY_FACTOR, KEY_MEASURES, LBORRES) %>%
+      tidyr::spread(key = "KEY_MEASURES", value = "LBORRES")),
     y = (data %>%
-      select(LBDTC, SUBJID, VISIT, DOMAIN, STUDYID, LBFAST, KEY_FACTOR) %>%
-      distinct()),
+      dplyr::select(LBDTC, SUBJID, VISIT, DOMAIN, STUDYID, LBFAST, KEY_FACTOR) %>%
+      dplyr::distinct()),
     by = c("KEY_FACTOR" = "KEY_FACTOR")
   ) %>%
-    mutate(
+    dplyr::mutate(
       VISIT = factor(
         x = VISIT,
         levels = c(
@@ -182,9 +138,9 @@ format_lb <- function(data) {
         )
       )
     ) %>%
-    group_by(DOMAIN, STUDYID, SUBJID) %>%
-    arrange(VISIT) %>%
-    select(-KEY_FACTOR) %>%
+    dplyr::group_by(DOMAIN, STUDYID, SUBJID) %>%
+    dplyr::arrange(VISIT) %>%
+    dplyr::select(-KEY_FACTOR) %>%
     data.frame()
 
   list(data = data_values, annotation = descriptive_data)
@@ -194,8 +150,8 @@ format_lb <- function(data) {
 compute_bmi <- function(data) {
   if (!"BMI"%in%data$annotation[["VSTESTCD"]]) {
     weight_unit <- data$annotation %>% 
-      filter(VSTESTCD == "WEIGHT") %>% 
-      select(VSORRESU) %>% 
+      dplyr::filter(VSTESTCD == "WEIGHT") %>% 
+      dplyr::select(VSORRESU) %>% 
       unlist()
     convert_weight_unit <- switch(
       EXPR = weight_unit,
@@ -204,8 +160,8 @@ compute_bmi <- function(data) {
     )
     
     height_unit <- data$annotation %>% 
-      filter(VSTESTCD=="HEIGHT") %>% 
-      select(VSORRESU) %>% 
+      dplyr::filter(VSTESTCD=="HEIGHT") %>% 
+      dplyr::select(VSORRESU) %>% 
       unlist()
     convert_height_unit <- switch(
       EXPR = height_unit,
@@ -214,7 +170,7 @@ compute_bmi <- function(data) {
       "in" = {function(x) (x * 2.54) / 100}
     )
     
-    data$data <- mutate(
+    data$data <- dplyr::mutate(
       .data = data$data,
       BMI = convert_weight_unit(WEIGHT) / convert_height_unit(HEIGHT)
     )
@@ -230,7 +186,7 @@ compute_bmi <- function(data) {
       row.names = 1L, 
       class = "data.frame"
     )
-    data$annotation <- bind_rows(data$annotation, default_CDISC_BMI)
+    data$annotation <- dplyr::bind_rows(data$annotation, default_CDISC_BMI)
   }
   data
 }
@@ -257,15 +213,15 @@ all_installed <- all(sapply(
 
 if (!all_installed) {
   # Install R packages and load them
-  if (!all(c("udunits2", "units", "devtools", "caTools")%in%rownames(installed.packages()))) {
+  if (!all(c("udunits2", "units", "devtools", "caTools")%in%rownames(utils::installed.packages()))) {
     utils::install.packages(
-      pkgs = setdiff(c("udunits2", "units", "devtools", "caTools"), rownames(installed.packages())),
+      pkgs = setdiff(c("udunits2", "units", "devtools", "caTools"), rownames(utils::installed.packages())),
       configure.args = "--with-udunits2-lib=/usr/local/lib",
       quiet = TRUE,
       repos = c("http://cran.us.r-project.org", "https://cran.rstudio.com/")
     )
   }
-  if (!all(c("opal")%in%rownames(installed.packages()))) {
+  if (!all(c("opal")%in%rownames(utils::installed.packages()))) {
     utils::install.packages(
       pkgs = "opal", 
       repos = c("https://cran.obiba.org", "https://cran.rstudio.com/"), 
@@ -273,7 +229,7 @@ if (!all_installed) {
       quiet = TRUE
     )
   }
-  list_packages <- read.table(
+  list_packages <- utils::read.table(
     col.names = c("package", "version"), 
     text = '"acepack"	"1.4.1"
     "assertthat"	"0.2.0"
@@ -435,19 +391,19 @@ if (!all_installed) {
 ###############
 # Do the funny stuff
 ###############
-o <- opal.login(
+o <- opal::opal.login(
   username = opal_credentials$opal_login,
   password = opal_credentials$opal_password,
   url = opal_credentials$opal_server
 )
 
+available_tables <- intersect(opal::opal.datasources(opal = o)[[1]]$table, c("DM", "VS", "LB", "MH", "CM"))
 
-available_tables <- intersect(opal.datasources(opal = o)[[1]]$table, c("DM", "VS", "LB", "APMH"))
 for (itable in available_tables) {
-  opal.assign(opal = o, symbol = itable, value = paste0("rhapsody.", itable))
-  assign(x = itable, value = opal.execute(o, itable))
+  opal::opal.assign(opal = o, symbol = itable, value = paste0("rhapsody.", itable))
+  assign(x = itable, value = opal::opal.execute(o, itable))
 }
-opal.logout(o)
+opal::opal.logout(o)
 rm(list = c("o", "itable", "available_tables"))
 
 
@@ -477,16 +433,16 @@ if (any(c(is(vs_tidy, "try-error"), is(vs_tidybmi, "try-error"), is(lb_tidy, "tr
 }
   
 
-DMVSLB <- full_join(
-  x = select(.data = mutate(.data = DM, VISIT = "BASELINE"), -DOMAIN),
-  y = full_join(
-    x = select(.data = mutate(.data = vs_tidy$data, VISIT = as.character(VISIT)), -DOMAIN),
-    y = select(.data = mutate(.data = lb_tidy$data, VISIT = as.character(VISIT)), -DOMAIN),
+DMVSLB <- dplyr::full_join(
+  x = dplyr::select(.data = dplyr::mutate(.data = DM, VISIT = "BASELINE"), -DOMAIN),
+  y = dplyr::full_join(
+    x = dplyr::select(.data = dplyr::mutate(.data = vs_tidy$data, VISIT = as.character(VISIT)), -DOMAIN),
+    y = dplyr::select(.data = dplyr::mutate(.data = lb_tidy$data, VISIT = as.character(VISIT)), -DOMAIN),
     by = c("STUDYID", "SUBJID", "VISIT")
   ),
   by = c("STUDYID", "SUBJID", "VISIT")
 ) %>%
-  mutate(
+  dplyr::mutate(
     VISIT = factor(
       x = VISIT,
       levels = c(
@@ -496,12 +452,12 @@ DMVSLB <- full_join(
       )
     )
   ) %>% 
-  group_by(STUDYID, SUBJID) %>%
-  filter(
+  dplyr::group_by(STUDYID, SUBJID) %>%
+  dplyr::filter(
     (VISIT=="BASELINE" & !is.na(AGE) & !is.na(SEX)) | VISIT!="BASELINE"
   ) %>% 
-  arrange(VISIT) %>% 
-  mutate(
+  dplyr::arrange(VISIT) %>% 
+  dplyr::mutate(
     AGE0 = AGE[1],
     BMI0 = BMI[1],
     SEX0 = as.numeric(SEX=="M")[1]
@@ -510,7 +466,7 @@ DMVSLB <- full_join(
 
 
 if ("FAMILYID" %in% colnames(APMH)) {
-  DMVSLB <- left_join(
+  DMVSLB <- dplyr::left_join(
     x = DMVSLB,
     y = APMH %>% 
       dplyr::select(FAMILYID, SUBJID, STUDYID) %>% 
@@ -533,12 +489,12 @@ if (
 
 if (
   DMVSLB %>%
-    group_by(SUBJID) %>%
-    summarise(n = n()) %>%
-    select(n) %>%
+    dplyr::group_by(SUBJID) %>%
+    dplyr::summarise(n = n()) %>%
+    dplyr::select(n) %>%
     all()
 ) {
-  DMVSLB <- mutate(
+  DMVSLB <- dplyr::mutate(
     .data = DMVSLB,
     VISIT_YEARS = as.numeric(
       difftime(as.Date(VSDTC), min(as.Date(VSDTC), na.rm = TRUE), unit = "weeks") / 52.25
