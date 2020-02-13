72be9a7f65dc08adb9b20c5e203cecdea8197b41
Mickaël Canouil <mickael.canouil@cnrs.fr>
2020-02-13 10:56:44 +0100

    use MRAN for packages version

--- docker/r_devtools.R
+++ docker/r_devtools.R
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 
 utils::install.packages(
-  pkgs = c('udunits2', 'units', 'devtools', 'caTools'),
+  pkgs = c('udunits2', 'units', 'remotes', 'devtools', 'caTools'),
   configure.args = '--with-udunits2-lib=/usr/local/lib',
   # repos = c('http://cran.us.r-project.org', 'https://cran.rstudio.com/'),
   quiet = TRUE
